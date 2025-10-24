9d3ad3aa854d0efacb0e5139b8b876b0f9ad994b
Nathan Silva de Souza <nathan_bzss@hotmail.com>
2025-10-24 20:58:21 -0300

    Definição da Classe

---
+++ Funcionario.py
@@ -0,0 +1,6 @@
+class Funcionario():
+    def __init__(self, nome, salarioBruto, totalAcrescimos, totalDescontos):
+        self.nome = nome
+        self.salarioBruto = salarioBruto
+        self.totalAcrescimos = totalAcrescimos
+        self.totalDescontos = totalDescontos

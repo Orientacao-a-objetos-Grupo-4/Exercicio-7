26b4e9394cc4e45d8f3057d6f09be94205626686
Nathan Silva de Souza <nathan_bzss@hotmail.com>
2025-10-24 21:16:29 -0300

    Finaliaição do Projeto

--- Funcionario.py
+++ Funcionario.py
@@ -4,3 +4,30 @@ class Funcionario():
         self.salarioBruto = salarioBruto
         self.totalAcrescimos = totalAcrescimos
         self.totalDescontos = totalDescontos
+
+    def setNome(self, nome):
+        self.nome = nome
+
+    def getNome(self):
+        return self.nome
+
+    def setSalarioBruto(self, salarioBruto):
+        self.salarioBruto = salarioBruto
+
+    def getSalarioBruto(self):
+        return self.salarioBruto
+
+    def setTotalAcrescimos(self, totalAcrescimos):
+        self.totalAcrescimos = totalAcrescimos
+
+    def getTotalAcrescimos(self):
+        return self.totalAcrescimos
+
+    def setTotalDescontos(self, totalDescontos):
+        self.totalDescontos = totalDescontos
+
+    def getTotalDescontos(self):
+        return self.totalDescontos
+
+    def calcularSalarioLiquido(self):
+        return self.salarioBruto + self.totalAcrescimos - self.totalDescontos
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from Funcionario import Funcionario
 
+if __name__ == '__main__':
+    print("Exercicio 7")
+
+f1 = Funcionario("Nathan", 1500, 80, 200)
+print(f1.calcularSalarioLiquido())
